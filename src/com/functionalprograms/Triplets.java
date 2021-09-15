3cf2f66ef2aa46719a2d21599e5415d0dae385dc
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 14:27:30 +0530

    Solved: Distinct Triplets Solution.

--- src/com/functionalprograms/Triplets.java
+++ src/com/functionalprograms/Triplets.java
@@ -1,10 +1,41 @@
 package com.functionalprograms;
 
+import java.util.Scanner;
+
 public class Triplets {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int findDistinctTriplets(int b[], int length) {
+		System.out.println();
+		int i, j, k, count = 0;
+		for (i = 0; i < length; i++) {
+			for (j = i + 1; j < length; j++) {
+				for (k = j + 1; k < length; k++) {
+					if (b[i] + b[j] + b[k] == 0) {
+						System.out.println(b[i] + " " + b[j] + " " + b[k]);
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+
+		System.out.print("Enter Size of Array :");
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("Enter " + (i + 1) + " Value :");
+			arr[i] = sc.nextInt();
+		}
+		
+		int sum = findDistinctTriplets(arr, size);
+		System.out.println("No.of Tripltes :" + sum);
+		System.out.println();
 	}
 
 }
